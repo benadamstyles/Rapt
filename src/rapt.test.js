e4e848d1e0198c40dc7bbcbaa0a085860b315546
Ben Styles <enquiries@leeds-ebooks.co.uk>
2018-02-09 11:17:55 +0000

    add more tests

--- src/rapt.test.js
+++ src/rapt.test.js
@@ -6,6 +6,7 @@ describe('isRapt', () => {
   it('returns true for a Rapt instance', () => {
     expect(isRapt(rapt({}))).toBe(true)
   })
+
   it('returns false for anything else', () => {
     expect(isRapt(1)).toBe(false)
     expect(isRapt('')).toBe(false)
@@ -13,5 +14,17 @@ describe('isRapt', () => {
     expect(isRapt({})).toBe(false)
     expect(isRapt()).toBe(false)
     expect(isRapt(rapt({}).val())).toBe(false)
+  })
+})
+
+describe('Rapt methods', () => {
+  describe('map', () => {
+    it('transforms the value as expected', () => {
+      expect(
+        rapt(10)
+          .map(String)
+          .val()
+      ).toBe('10')
+    })
   })
 })
